dab16066fe228762fd59096ab592d003be4f00cc
ajayqservices <ajay@qservicesit.com>
2016-11-04 15:15:37 +0530

    sqllite

--- Desktop/marshmallow-permissions-example-master/Sqllite/app/src/main/java/com/ajay/developer/sqllite/MainActivity.java
+++ Desktop/marshmallow-permissions-example-master/Sqllite/app/src/main/java/com/ajay/developer/sqllite/MainActivity.java
@@ -1,11 +1,21 @@
 package com.ajay.developer.sqllite;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     Sqllite sqlite;
+    Button insert, delete, update, fetch, getCount;
+    SQLiteDatabase db;
+    Cursor c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +23,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         sqlite = new Sqllite(this);
         sqlite.getWritableDatabase();
+        getCount = (Button) findViewById(R.id.count);
+        getCount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int b = new Sqllite(MainActivity.this).numberOfRows();
+                Toast.makeText(MainActivity.this, "" + b, Toast.LENGTH_SHORT).show();
 
-        int a = new Sqllite(MainActivity.this).numberOfRows();
-        Toast.makeText(this, "" + a, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        insert = (Button) findViewById(R.id.insert);
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean a = new Sqllite(MainActivity.this).Insertdata("ajay", "123", "ajay", "a");
+                Toast.makeText(MainActivity.this, "" + a, Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+        fetch = (Button) findViewById(R.id.fetch);
+        fetch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                c = new Sqllite(MainActivity.this).getData();
+                //Toast.makeText(MainActivity.this, "" + c.getCount(), Toast.LENGTH_SHORT).show();
+
+                ArrayList<String> empName = new ArrayList<String>();
+                if (c.moveToFirst()) {
+
+                    while (c.isAfterLast() == false) {
+                        String name = c.getString(c
+                                .getColumnIndex("id"));
+
+                        empName.add(name);
+                        c.moveToNext();
+                    }
+                }
+                Toast.makeText(MainActivity.this, "" + empName.size(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
--- Desktop/marshmallow-permissions-example-master/Sqllite/app/src/main/java/com/ajay/developer/sqllite/Sqllite.java
+++ Desktop/marshmallow-permissions-example-master/Sqllite/app/src/main/java/com/ajay/developer/sqllite/Sqllite.java
@@ -1,6 +1,8 @@
 package com.ajay.developer.sqllite;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.DatabaseErrorHandler;
 import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
@@ -14,9 +16,9 @@ import android.widget.Toast;
 public class Sqllite extends SQLiteOpenHelper {
 
 
-    private static String DbName = "test";
-    private static int DbVersion = 1;
-    private static String DbTableName = "joshi";
+    private static String DbName = "test1";
+    private static int DbVersion = 4;
+    private static String DbTableName = "joshi1";
     Context context;
 
     public Sqllite(Context context) {
@@ -27,22 +29,57 @@ public class Sqllite extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + DbTableName + "(id integrer primary key, name text,phone text,email text,street text)");
+        db.execSQL("CREATE TABLE " + DbTableName + "(id INTEGER PRIMARY KEY AUTOINCREMENT, name text,phone text,email text,street text)");
+
         Toast.makeText(context, "Db created", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS" + DbTableName + "");
+        db.execSQL("DROP TABLE IF EXISTS " + DbTableName + "");
+        onCreate(db);
         Toast.makeText(context, "Db updated", Toast.LENGTH_SHORT).show();
 
     }
 
     public int numberOfRows() {
         SQLiteDatabase db = this.getReadableDatabase();
-        int numRows = (int) DatabaseUtils.queryNumEntries(db, DbTableName);
-        return numRows;
+        int row = (int) DatabaseUtils.queryNumEntries(db, DbTableName);
+        return row;
     }
 
 
+    public boolean Insertdata(String name, String phone, String email, String street) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", name);
+        contentValues.put("phone", phone);
+        contentValues.put("email", email);
+        contentValues.put("street", street);
+        db.insert(DbTableName, null, contentValues);
+        return true;
+    }
+
+    public Cursor getData() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery("select * from " + DbTableName, null);
+        return res;
+    }
+
+    public void abc() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("", "");
+        db.insert(DbTableName, null, cv);
+
+        SQLiteDatabase db1 = this.getReadableDatabase();
+        int a = (int) DatabaseUtils.queryNumEntries(db1, DbTableName);
+
+        SQLiteDatabase db2 = this.getReadableDatabase();
+        Cursor c = db2.rawQuery("SELECT * From " + DbTableName, null);
+
+    }
 }
+
+
